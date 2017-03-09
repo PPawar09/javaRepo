ad3d9704a5ab7dd484f6bd5339b68355ae47751d
PPawar09 <prakash09pawar@gmail.com>
2017-03-08 23:34:31 -0500

    jdbc tested client.

---
+++ CoreJavaBasic/src/com/pp/corejava/ExceptionFormatUtil.java
@@ -0,0 +1,53 @@
+package com.pp.corejava;
+
+import java.text.MessageFormat;
+
+public class ExceptionFormatUtil {
+	
+	
+	/**
+	 * Take the error message template and insert the class and method name into the message and return as a String.
+	 * @return String Formatted message for the Exception type specified by the caller.
+	 */
+	public static String getMessage(final String messageType) {
+		return getStackTraceMessage(messageType);
+	}
+
+	/**
+	 * Common method used to get the stack trace data and format it. Used by the public methods in this class which can
+	 * decorate the message returned.
+	 * 
+	 * @param messageType
+	 * @return
+	 */
+	private static final String getStackTraceMessage(final String messageType) {
+		final StackTraceElement stackTraceElements[] = getStackTraceElements();
+		final StringBuffer exceptionCaughtLocation = new StringBuffer(100);
+		exceptionCaughtLocation.append(stackTraceElements[3].getClassName());
+		exceptionCaughtLocation.append('.');
+		exceptionCaughtLocation.append(stackTraceElements[3].getMethodName());
+		final MessageFormat formatter = new MessageFormat(messageType);
+		final Object[] parameter = new Object[] { exceptionCaughtLocation.toString() };
+		final String errorMessage = formatter.format(parameter);
+		return errorMessage;
+	}
+
+	/**
+	 * Utility method to return the class name where the exception is caught.
+	 * 
+	 * @return
+	 */
+	public static final String getMethodName() {
+		final StackTraceElement stackTraceElements[] = getStackTraceElements();
+		return stackTraceElements[2].getMethodName();
+	}
+
+	/**
+	 * Returns an array of the stack trace elements for use by other methods in this class.
+	 * @return
+	 */
+	private static StackTraceElement[] getStackTraceElements() {
+		return (new Throwable()).getStackTrace();
+	}
+
+}
--- CoreJavaBasic/src/com/pp/corejava/JDBCSteps.java
+++ CoreJavaBasic/src/com/pp/corejava/JDBCSteps.java
@@ -1,66 +1,98 @@
 package com.pp.corejava;
 
-import java.io.ByteArrayInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.Timestamp;
-import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+
+/**
+ * This is Java test client class for steps of database connectivity from Java Application.
+ * It is having different method using JDBC driver based on the database.
+ * 
+ * There are few example performing DB CRUD operation using Statemen or PreparedStatment API.
+ * 
+ * High Level Steps for JDBC Connection are 
+ * step1 register or load the driver based on the DB : Object obj = Class.forName("oracle.jdbc.driver.OracleDriver");
+ * Step2 create DB Connection. : DriverManager.getConnection(url);
+ * Step3 create Statement from connection.
+ * Step4 execute query from Statement.
+ * Step5 Process the ResultSet.
+ *
+ * @author  Prakash Pawar
+ */
 public class JDBCSteps {
 	
 	static Connection con = null;
+	static Logger logger = Logger.getLogger(JDBCSteps.class.getName());
+	
 	public static void main(String ags[]){
 		
 		try{
-			
 			JDBCSteps js = new JDBCSteps();
-			js.connect2MsSqlDb();
 			
+			//getting connection for MS SQL Server DB.
+			con = js.connect2MsSqlDb();
 			
-			//step1 register or load the driver based on the DB
-			Object obj = Class.forName("oracle.jdbc.driver.OracleDriver");
+			// Insert data into Table EMPLOYEE
+			PreparedStatement  preparedStatement = con.prepareStatement("INSERT INTO DBO.EMPLOYEE (EMP_ID,EMP_NAME)"
+					+ "VALUES (?, ?)");
+			preparedStatement.setInt(1,14252);
+			preparedStatement.setString(2,"Vishakha");
+			preparedStatement.executeUpdate();  // data will be inserted in table.
 			
-			//Step2 create DB Connection.
-			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","oracle");
-			
-			// Option 1
-			//Step3 Create Statement if using it prepferred way is use PreparedStatement.
-			Statement stmt = con.createStatement();
-			
-			//step 4
-			ResultSet rs=stmt.executeQuery("select * from emp");
+			// Select data from table.
+			//String selectSQL = "SELECT * FROM DBO.EMPLOYEE";
+			String selectSQL = "SELECT * FROM DBO.EMPLOYEE WHERE EMP_ID = ?";
+			PreparedStatement  preparedStmnt = con.prepareStatement(selectSQL);
+			preparedStmnt.setInt(1,14252);
+			ResultSet rs = preparedStmnt.executeQuery(selectSQL);
 			
 			// read the data from table.
 			while(rs.next()){  
 				System.out.println(rs.getInt(1)+" "+rs.getString(2));  
-				}  
+			} 
 			
-			// Option 2 by Using PreparedStatement
-			//They are pre-compiled (once), so faster for repeated execution of dynamic SQL (where parameters change)
-			
-			//Step 3
-			PreparedStatement  preparedStatement = con.prepareStatement("INSERT INTO Person (name, email, birthdate, photo) "
-					+ "VALUES (?, ?, ?, ?)");
-			preparedStatement.setString(1, "name");
-			preparedStatement.setString(2, "emailId");
-			preparedStatement.setTimestamp(3, new Timestamp(new Date().getTime()));
-			preparedStatement.setBinaryStream(4, new ByteArrayInputStream("myString".getBytes()));
-			//preparedStatement.executeUpdate();  // for insert update or delete.
-			ResultSet rst = preparedStatement.executeQuery(); // for select query
 			con.close();
 			preparedStatement.close();
+			
 		}catch(Exception e){
-			e.printStackTrace();
+			// instead of using printStack Trace this utility format the message
+			logger.log(Level.INFO,"Exception|Class=JDBCSteps " + ExceptionFormatUtil.getMessage(e.getMessage()));
 		}
 	}
 	
-	public void connect2MsSqlDb() throws Exception{
-		String url = "jdbc:sqlserver://MYPC/MYDB;databaseName=SQLEXPRESS;integratedSecurity=true";
+	/**
+	 * This method connect to MS SQL Server Database and return connection object
+	 * @return
+	 * @throws Exception
+	 */
+	public Connection connect2MsSqlDb() throws Exception{
+		//String url = "jdbc:sqlserver://MYPC/MYDB;databaseName=SQLEXPRESS;integratedSecurity=true";
+		//String url = "jdbc:sqlserver://localhost:1433;databaseName=playsoft;user=demo;password=password";
+	    // String url = "jdbc:sqlserver://localhost:1433;databaseName=playsoft;integratedSecurity=true";
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=playsoft;user=demo;password=password";
+		
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 		Connection conn = DriverManager.getConnection(url);
-		System.out.println("connected");
+		logger.log(Level.INFO,"Class=JDBCSteps|Method=connect2MsSqlDb| Connected to SQL Server");
+		
+		return conn;
 	}
+	
+	/**
+	 * This method connect to Oracle Server Database and return connection object
+	 * @return
+	 * @throws Exception
+	 */
+	public Connection connect2OracleDb() throws Exception{
+		//step1 register or load the driver based on the DB
+		Object obj = Class.forName("oracle.jdbc.driver.OracleDriver");
+		//Step2 create DB Connection.
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","oracle");
+		return con;
+	}
+
 }

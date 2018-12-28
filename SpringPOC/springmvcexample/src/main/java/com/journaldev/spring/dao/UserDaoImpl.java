d68ebbe7d8165621ca7f2ee6a894320092b81206
Prakash Pawar <prakash09pawar@gmail.com>
2018-12-28 10:38:44 +0530

    Added Spring JDBC

---
+++ SpringPOC/springmvcexample/src/main/java/com/journaldev/spring/dao/UserDaoImpl.java
@@ -0,0 +1,54 @@
+package com.journaldev.spring.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import com.journaldev.spring.model.Login;
+import com.journaldev.spring.model.User;
+
+public class UserDaoImpl implements UserDao {
+	@Autowired
+	DataSource datasource;
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public void register(User user) {
+		String sql = "insert into users values(?,?,?,?,?,?,?)";
+		jdbcTemplate.update(sql, new Object[] { user.getUserName(), user.getPassword(), user.getFirstname(),
+				user.getLastname(), user.getEmail(), user.getAddress(), user.getPhone() });
+	}
+
+	public User validateUser(Login login) {
+		String sql = "select * from users where username='" + login.getUsername() + "' and password='" + login.getPassword()
+				+ "'";
+		List<User> users = jdbcTemplate.query(sql, new ResultSetExtractor<List<User>>() {
+			List<User> listUser = new ArrayList<User>();
+			public List<User> extractData(ResultSet rs)
+					throws SQLException, DataAccessException {
+				while (rs.next()) {
+					User user = new User();
+					user.setFirstname(rs.getString("firstname"));
+					user.setLastname(rs.getString("lastname"));
+					user.setEmail(rs.getString("email"));
+					user.setAddress(rs.getString("address"));
+					user.setPhone(rs.getInt("phone"));
+					
+					listUser.add(user);
+				}
+				return listUser;
+			}
+		});
+		return users.size() > 0 ? users.get(0) : null;
+	}
+}
+
+
--- SpringPOC/springmvcexample/src/main/java/com/journaldev/spring/model/User.java
+++ SpringPOC/springmvcexample/src/main/java/com/journaldev/spring/model/User.java
@@ -3,13 +3,56 @@ package com.journaldev.spring.model;
 public class User {
 
 	private String userName;
-
+	private String password;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String address;
+	private int phone;
+	
 	public String getUserName() {
 		return userName;
 	}
-
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	
 	
 }

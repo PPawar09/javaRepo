50d1debec993538796e4453c8acc5814330bce61
Prakash Pawar <prakash09pawar@gmail.com>
2019-01-02 12:27:36 +0530

    added Mockito Api

---
+++ junit/src/main/java/com/mokito/dao/DatabaseDAO.java
@@ -0,0 +1,8 @@
+package com.mokito.dao;
+
+public class DatabaseDAO
+{
+    public void save(String fileName) {
+        System.out.println("Saved in database");
+    }
+}
---
+++ junit/src/main/java/com/mokito/dao/NetworkDAO.java
@@ -0,0 +1,8 @@
+package com.mokito.dao;
+
+public class NetworkDAO
+{
+    public void save(String fileName) {
+        System.out.println("Saved in network location");
+    }
+}

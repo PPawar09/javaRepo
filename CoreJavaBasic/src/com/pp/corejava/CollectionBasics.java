67f238401360bc9c6d277761a404c79603f2adfd
Prakash Pawar <prakash09pawar@gmail.com>
2019-03-15 11:58:39 +0530

    added stuff-14 March 2019

--- CoreJavaBasic/src/com/pp/corejava/CollectionBasics.java
+++ CoreJavaBasic/src/com/pp/corejava/CollectionBasics.java
@@ -3,10 +3,10 @@ package com.pp.corejava;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,6 +32,9 @@ public class CollectionBasics {
 		cb.SortingCollections();
 		//custom object sorting by implementing compareTo of Interface Comaparable
 		cb.testSortStudent(); 
+		
+		System.out.println("*** Play LinkedList ***");
+		cb.playLinkedList();
 
 	}
 
@@ -201,6 +204,47 @@ public class CollectionBasics {
 		Collections.sort(listStudent); // This using Comparable Interface for sorting Collections
 		System.out.println("***StudentList Display After Name Sort " + listStudent);
 	}
+	
+	public void playLinkedList(){
+		
+		// Creating object of class linked list 
+        LinkedList<String> object = new LinkedList<String>();
+        
+        // Adding elements to the linked list 
+        object.add("A"); 
+        object.add("B"); 
+        object.addLast("C"); 
+        object.addFirst("D"); 
+        object.add(2, "E"); 
+        object.add("F"); 
+        object.add("G"); 
+        System.out.println("Linked list : " + object); 
+  
+        // Removing elements from the linked list 
+        object.remove("B"); 
+        object.remove(3); 
+        object.removeFirst(); 
+        object.removeLast(); 
+        System.out.println("Linked list after deletion: " + object); 
+  
+        // Finding elements in the linked list 
+        boolean status = object.contains("E"); 
+  
+        if(status) 
+            System.out.println("List contains the element 'E' "); 
+        else
+            System.out.println("List doesn't contain the element 'E'"); 
+  
+        // Number of elements in the linked list 
+        int size = object.size(); 
+        System.out.println("Size of linked list = " + size); 
+  
+        // Get and set elements from linked list 
+        Object element = object.get(2); 
+        System.out.println("Element returned by get() : " + element); 
+        object.set(2, "Y"); 
+        System.out.println("Linked list after change : " + object); 
+    } 
 }
 
 /**
--- CoreJavaBasic/src/com/pp/dspattern/Singleton.java
+++ CoreJavaBasic/src/com/pp/dspattern/Singleton.java
@@ -47,3 +47,17 @@ class ConnectionSingle {
 	}
 
 }
+
+class ThreadSafeSingleton {
+
+	private static ThreadSafeSingleton instance;
+
+	private ThreadSafeSingleton(){}
+
+	public static synchronized ThreadSafeSingleton getInstance(){
+		if(instance == null){
+			instance = new ThreadSafeSingleton();
+		}
+		return instance;
+	}
+}

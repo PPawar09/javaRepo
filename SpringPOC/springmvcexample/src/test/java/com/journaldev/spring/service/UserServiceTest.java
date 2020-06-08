f9a7fa31b48d260a7cc9e294cd52e519fc42783d
Prakash Pawar <prakash09pawar@gmail.com>
2020-06-08 09:52:23 +0530

    update

---
+++ SpringPOC/springmvcexample/src/test/java/com/journaldev/spring/service/UserServiceTest.java
@@ -0,0 +1,39 @@
+package com.journaldev.spring.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.journaldev.spring.dao.UserDaoImpl;
+import com.journaldev.spring.model.UserTaskRecord;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+	
+	@Mock
+	UserDaoImpl userDaoImplMock;
+
+	@InjectMocks
+	UserService userService;
+	
+	@Test
+	public void testFindTheGreatestFromAllData() {
+		
+		List<UserTaskRecord> utrList = null;
+		
+		when(userDaoImplMock.getTaskById("123")).thenReturn(utrList);
+		assertEquals(24, userService.getTaskByUserId("123"));
+	}
+	
+
+	
+	
+
+}

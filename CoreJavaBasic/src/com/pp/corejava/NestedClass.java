4cda5bb3b4a5d6dfef6dbf23320ce8ee4feca9ee
Prakash Pawar <prakash09pawar@gmail.com>
2020-06-30 15:33:47 +0530

    added topic

---
+++ CoreJavaBasic/src/com/pp/corejava/NestedClass.java
@@ -0,0 +1,17 @@
+package com.pp.corejava;
+
+//Ref : http://tutorials.jenkov.com/java/nested-classes.html
+
+/*
+ You can create several different types of nested classes in Java. The different Java nested class types are:
+
+	Static nested classes
+	Non-static nested classes : I have used this for nested POJO in JB2
+	Local classes
+	Anonymous classes
+
+*/
+
+public class NestedClass {
+
+}
